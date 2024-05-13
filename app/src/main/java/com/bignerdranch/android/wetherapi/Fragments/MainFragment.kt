122a3b4aaa919be3ce20dd492520191764bab46c
Anryurev <137984074+Anryurev@users.noreply.github.com>
2024-05-13 22:25:43 +0700

    Поменял цвет Status Bar. Создал HoursFragment и DaysFragment. Создал VpAdapter. Подключил адаптер к ViewPage2 и TabLayout (MainFragment).

--- app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/MainFragment.kt
+++ app/src/main/java/com/bignerdranch/android/wetherapi/Fragments/MainFragment.kt
@@ -9,11 +9,23 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.setFragmentResultListener
 import com.bignerdranch.android.wetherapi.R
+import com.bignerdranch.android.wetherapi.adapters.VpAdapter
 import com.bignerdranch.android.wetherapi.databinding.ActivityMainBinding
 import com.bignerdranch.android.wetherapi.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment: Fragment(){
+    private val fList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val tList = listOf(
+        "Hours",
+        "Days"
+    )
     private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
@@ -27,6 +39,15 @@ class MainFragment: Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, fList)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
